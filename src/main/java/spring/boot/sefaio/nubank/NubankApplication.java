3e8753e448c1020b4b62e4ebc07c5a4fd5f2069d
Andherson Costa <00andherson@gmail.com>
2025-07-01 11:13:03 -0300

    initial commit/config prop

---
+++ src/main/java/spring/boot/sefaio/nubank/NubankApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.sefaio.nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/sefaio/nubank/NubankApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.sefaio.nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
